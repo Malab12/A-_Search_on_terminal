4156186d8f7c2a2754816c54e5a1afad4ec1ed4d
Malab_i3Gaming <malab@outlook.com>
2022-12-14 10:12:30 +0530

    Adding nodes to the open vector

--- main.cpp
+++ main.cpp
@@ -8,8 +8,9 @@ using std::ifstream;
 using std::istringstream;
 using std::string;
 using std::vector;
+using std::abs;
 
-enum class State {kEmpty, kObstacle};
+enum class State {kEmpty, kObstacle, kClosed};
 
 
 vector<State> ParseLine(string line) {
@@ -41,9 +42,17 @@ vector<vector<State>> ReadBoardFile(string path) {
   return board;
 }
 
-// TODO: Write the Heuristic function here.
+
+// Calculate the manhattan distance
 int Heuristic(int x1, int y1, int x2, int y2) {
-    return abs(x2 - x1) + abs(y2 - y1);
+  return abs(x2 - x1) + abs(y2 - y1);
+}
+
+// TODO: Write the AddToOpen function here.
+void AddToOpen(int x, int y, int g, int h, vector<vector<int>> &open, vector<vector<State>> &board) {
+    vector<int> node{x, y, g, h};
+    open.push_back(node);
+    board[x][y] = State::kClosed;
 }
 
 /** 
@@ -74,6 +83,28 @@ void PrintBoard(const vector<vector<State>> board) {
   }
 }
 
+
+void PrintVectorOfVectors(vector<vector<int>> v) {
+  for (auto row : v) {
+    cout << "{ ";
+    for (auto col : row) {
+      cout << col << " ";
+    }
+    cout << "}" << "\n";
+  }
+}
+
+void PrintVectorOfVectors(vector<vector<State>> v) {
+  for (auto row : v) {
+    cout << "{ ";
+    for (auto col : row) {
+      cout << CellString(col) << " ";
+    }
+    cout << "}" << "\n";
+  }
+}
+
+
 void TestHeuristic() {
   cout << "----------------------------------------------------------" << "\n";
   cout << "Heuristic Function Test: ";
@@ -90,8 +121,52 @@ void TestHeuristic() {
   } else {
     cout << "passed" << "\n";
   }
-  cout << "----------------------------------------------------------" << "\n";
+  return;
 }
+
+
+void TestAddToOpen() {
+  cout << "----------------------------------------------------------" << "\n";
+  cout << "AddToOpen Function Test: ";
+  int x = 3;
+  int y = 0;
+  int g = 5;
+  int h = 7;
+  vector<vector<int>> open{{0, 0, 2, 9}, {1, 0, 2, 2}, {2, 0, 2, 4}};
+  vector<vector<int>> solution_open = open; 
+  solution_open.push_back(vector<int>{3, 0, 5, 7});
+  vector<vector<State>> grid{{State::kClosed, State::kObstacle, State::kEmpty, State::kEmpty, State::kEmpty, State::kEmpty},
+                            {State::kClosed, State::kObstacle, State::kEmpty, State::kEmpty, State::kEmpty, State::kEmpty},
+                            {State::kClosed, State::kObstacle, State::kEmpty, State::kEmpty, State::kEmpty, State::kEmpty},
+                            {State::kEmpty, State::kObstacle, State::kEmpty, State::kEmpty, State::kEmpty, State::kEmpty},
+                            {State::kEmpty, State::kEmpty, State::kEmpty, State::kEmpty, State::kObstacle, State::kEmpty}};
+  vector<vector<State>> solution_grid = grid;
+  solution_grid[3][0] = State::kClosed;
+  AddToOpen(x, y, g, h, open, grid);
+  if (open != solution_open) {
+    cout << "failed" << "\n";
+    cout << "\n";
+    cout << "Your open list is: " << "\n";
+    PrintVectorOfVectors(open);
+    cout << "Solution open list is: " << "\n";
+    PrintVectorOfVectors(solution_open);
+    cout << "\n";
+  } else if (grid != solution_grid) {
+    cout << "failed" << "\n";
+    cout << "\n";
+    cout << "Your grid is: " << "\n";
+    PrintVectorOfVectors(grid);
+    cout << "\n";
+    cout << "Solution grid is: " << "\n";
+    PrintVectorOfVectors(solution_grid);
+    cout << "\n";
+  } else {
+    cout << "passed" << "\n";
+  }
+  cout << "----------------------------------------------------------" << "\n";
+  return;
+}
+
 
 int main() {
   int init[2]{0, 0};
@@ -101,4 +176,5 @@ int main() {
   PrintBoard(solution);
   // Tests
   TestHeuristic();
+  TestAddToOpen();
 }
